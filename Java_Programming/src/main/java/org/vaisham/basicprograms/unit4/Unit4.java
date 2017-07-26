e979e0e207a32c7fdcb5e18fae892150d5afffbb
Vaishampayan Reddy <vaisham92@gmail.com>
2017-07-25 22:10:13 -0700

    Unit4 programs

---
+++ Java_Programming/src/main/java/org/vaisham/basicprograms/unit4/Unit4.java
@@ -0,0 +1,49 @@
+package org.vaisham.basicprograms.unit4;
+
+public class Unit4 {
+
+    /**
+     * compare two character arrays for equality.
+     * return true if both the inputs are same
+     *
+     *
+     * @param input1
+     * @param input2
+     * @return
+     */
+    public boolean isEqual(char[] input1, char[] input2) {
+        return true;
+    }
+
+    /**
+     * given two Strings
+     * compare both the Strings and return true if
+     * they are equal
+     * and return false if
+     * they are unequal
+     *
+     *
+     * @param input1
+     * @param input2
+     * @return
+     */
+    public boolean isEqual(String input1, String input2) {
+        return true;
+    }
+
+    /**
+     * given a string,
+     *
+     * return true if the string is palindrome
+     * or false if it is not
+     *
+     * eg: madam -> return true
+     * eg: apple -> return false
+     *
+     * @param input
+     * @return
+     */
+    public boolean isPalindrome(String input) {
+        return false;
+    }
+}
