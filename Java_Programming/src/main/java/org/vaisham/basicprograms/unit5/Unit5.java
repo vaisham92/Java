a05e1d7034723b747d7d8930e4b6535ba8c28629
Vaishampayan Reddy <vaisham92@gmail.com>
2017-07-29 19:57:28 -0700

    adding unit5

---
+++ Java_Programming/src/main/java/org/vaisham/basicprograms/unit5/Unit5.java
@@ -0,0 +1,41 @@
+package org.vaisham.basicprograms.unit5;
+
+public class Unit5 {
+
+    /**
+     * given a positive integer convert the number to binary form
+     *
+     * eg:
+     *
+     * 2 -> return "10"
+     * 3 -> return "11"
+     * 10 -> return "1010"
+     * 0 -> return "0"
+     *
+     * @param number
+     * @return
+     */
+    public String toBinaryString(int number) {
+        return null;
+    }
+
+    /**
+     * given a binaryString convert
+     * the given string to its integer format
+     * and return the value
+     *
+     * eg:
+     *
+     * "10" -> return 2
+     * "11" -> return 3
+     * "1010" -> return 10
+     * "0" -> return 0
+     *
+     *
+     * @param binaryString
+     * @return
+     */
+    public int toInteger(String binaryString) {
+        return -1;
+    }
+}
